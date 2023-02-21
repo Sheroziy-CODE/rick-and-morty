1706bd8c067165e4a7ce06820e58f814c1bde8c3
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-02-21 13:57:56 +0100

    Added new Unit test for repository and ViewModel and Set up Pipe for unit tests

---
+++ app/src/test/java/rick_and_morty/data/repository/CharacterRepositoryTest.kt
@@ -0,0 +1,5 @@
+package rick_and_morty.data.repository
+
+import org.junit.Assert.*
+
+class CharacterRepositoryTest
---
+++ app/src/test/java/rick_and_morty/ui/characters/CharactersViewModelTest.kt
@@ -0,0 +1,16 @@
+package rick_and_morty.ui.characters
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class CharactersViewModelTest {
+
+    @Test
+    fun getCharacters() {
+    }
+
+    @Test
+    fun testGetCharacters() {
+    }
+}
