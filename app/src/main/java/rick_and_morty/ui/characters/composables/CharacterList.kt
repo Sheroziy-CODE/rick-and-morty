f79d1ca9ba1c6acb041842f13f3a68f77cf47392
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-03-03 22:01:48 +0100

    Added ScrollListener, to know if the user at the bottom, helped to get rid of multiple calles of the item, and saving state during rotation

--- app/src/main/java/rick_and_morty/ui/characters/composables/CharacterList.kt
+++ app/src/main/java/rick_and_morty/ui/characters/composables/CharacterList.kt
@@ -1,28 +1,46 @@
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import rick_and_morty.ui.characters.CharactersViewModel
 
 @Composable
 fun CharacterList(charactersViewModel: CharactersViewModel = viewModel(modelClass = CharactersViewModel::class.java)) {
 
     val characters = charactersViewModel.characters.collectAsState(emptyList())
-    LazyColumn (
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
+    val scrollListState = rememberLazyListState()
 
-        items(characters.value) {characters ->
+    LazyColumn (
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        state = scrollListState
+    ) {
+        items(characters.value) { characters ->
             CharacterRow(characterResultsDto = characters)
         }
-        item {
-            LaunchedEffect(true) {
-                    charactersViewModel.getCharacters()
-          }
+    }
+    val userIsAtBottom by remember{
+        derivedStateOf {
+            val layoutInfo = scrollListState.layoutInfo
+            val visibleItemsInfo = layoutInfo.visibleItemsInfo
+            if (layoutInfo.totalItemsCount == 0) {
+                false
+            } else {
+                val lastVisibleItem = visibleItemsInfo.last()
+                val viewportHeight = layoutInfo.viewportEndOffset + layoutInfo.viewportStartOffset
+                (lastVisibleItem.index + 1 == layoutInfo.totalItemsCount &&
+                        lastVisibleItem.offset + lastVisibleItem.size <= viewportHeight)
+            }
+        }
+    }
+    if (userIsAtBottom){
+        LaunchedEffect(true) {
+            charactersViewModel.getCharacters()
         }
     }
 }
