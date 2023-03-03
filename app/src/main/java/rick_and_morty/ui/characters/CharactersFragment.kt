b42c298c6d2423bf0439d3d3a9b1e4f45493fe8d
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-03-03 21:44:54 +0100

    Added dispose composition when lifecycle is destroyed

--- app/src/main/java/rick_and_morty/ui/characters/CharactersFragment.kt
+++ app/src/main/java/rick_and_morty/ui/characters/CharactersFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +18,7 @@ class CharactersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 CharacterList()
             }
