dd7564b83f6d4706f6d903070ad95dd3fd6c69d9
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-03-05 11:08:09 +0100

    Check

--- app/src/main/java/rick_and_morty/ui/characters/CharactersViewModel.kt
+++ app/src/main/java/rick_and_morty/ui/characters/CharactersViewModel.kt
@@ -27,7 +27,6 @@ class CharactersViewModel @Inject constructor(
         initialLoad = false
         getCharacters()
     }
-
     fun getCharacters() {
         if (!initialLoad) {
             viewModelScope.launch {
