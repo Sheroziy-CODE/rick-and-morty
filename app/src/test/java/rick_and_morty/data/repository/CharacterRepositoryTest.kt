602ec89a1c5b125d601cf3b2955ef2aca0b524fd
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-04-04 10:12:30 +0200

    Created Unit Test for getCahracterDetails

--- app/src/test/java/rick_and_morty/data/repository/CharacterRepositoryTest.kt
+++ app/src/test/java/rick_and_morty/data/repository/CharacterRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import rick_and_morty.data.model.CharacterResultsDto
 import rick_and_morty.data.model.RickAndMortyResponseDto
 import rick_and_morty.di.RickAndMortyApiRemoteDataSource
 
@@ -12,6 +13,7 @@ class CharacterRepositoryTest {
     private val rickAndMortyApiRemoteDataSource: RickAndMortyApiRemoteDataSource = mock ()
     private val classToTest = CharacterRepository(rickAndMortyApiRemoteDataSource)
     private val rickAndMorty: RickAndMortyResponseDto = mock ()
+    private val characterResult: CharacterResultsDto = mock ()
 
     @Test
     fun `verify getCharacter has been called`(): Unit = runBlocking {
@@ -21,5 +23,18 @@ class CharacterRepositoryTest {
         classToTest.getCharacters(1)
 
         verify(rickAndMortyApiRemoteDataSource).fetchRickAndMortyData(1)
+
+    }
+
+
+    @Test
+    fun `verify getCharacterDetails has been called`(): Unit = runBlocking {
+
+        given(rickAndMortyApiRemoteDataSource.fetchCharacterDetailsData(1)).willReturn(characterResult)
+
+        classToTest.getCharacterDetails(1)
+
+        verify(rickAndMortyApiRemoteDataSource).fetchCharacterDetailsData(1)
+
     }
 }
