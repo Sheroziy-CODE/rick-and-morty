6d7bf8d226c68af957f9b1ac7885a6afe48ade86
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-04-04 11:47:43 +0200

    Updated syntax in repository

--- app/src/main/java/rick_and_morty/data/repository/CharacterRepository.kt
+++ app/src/main/java/rick_and_morty/data/repository/CharacterRepository.kt
@@ -2,19 +2,16 @@ package rick_and_morty.data.repository
 
 import rick_and_morty.di.RickAndMortyApiRemoteDataSource
 import javax.inject.Inject
-import rick_and_morty.data.model.CharacterResultsDto
 
 
 class CharacterRepository @Inject constructor(
     private val rickAndMortyApiRemoteDataSource: RickAndMortyApiRemoteDataSource
-){
-    
-   suspend fun getCharacters(page: Int): List<CharacterResultsDto> {
-       return  rickAndMortyApiRemoteDataSource.fetchRickAndMortyData(page).results
-    }
+) {
+
+    suspend fun getCharacters(page: Int) =
+        rickAndMortyApiRemoteDataSource.fetchRickAndMortyData(page).results
 
 
-    suspend fun getCharacterDetails(id: Int): CharacterResultsDto {
-        return rickAndMortyApiRemoteDataSource.fetchCharacterDetailsData(id)
-    }
+    suspend fun getCharacterDetails(id: Int) =
+        rickAndMortyApiRemoteDataSource.fetchCharacterDetailsData(id)
 }
