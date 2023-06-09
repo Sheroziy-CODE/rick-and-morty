acc39d585d950fa6175f9ab8c9e292ec0d1e2cbb
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-06-09 10:26:16 +0200

    Renamed Dto for episodes

--- app/src/main/java/rick_and_morty/data/model/episodes/EpisodesResponseDto.kt
+++ app/src/main/java/rick_and_morty/data/model/episodes/EpisodesResponseDto.kt
@@ -2,13 +2,11 @@ package rick_and_morty.data.model.episodes
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import rick_and_morty.data.model.CharacterResultsDto
-import rick_and_morty.data.model.InfoDto
 
 @JsonClass(generateAdapter = true)
-data class EpisodeResponseDto(
+data class EpisodesResponseDto(
     @Json(name = "info")
     val infoDto: EpisodesInfoDto,
     @Json(name = "results")
-    val results: List<EpisodesResultDto>
+    val results: List<EpisodeResultDto>
 )
