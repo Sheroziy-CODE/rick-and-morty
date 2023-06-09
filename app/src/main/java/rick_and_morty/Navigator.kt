935a20985bfcbf811477f23cde50bfee298d9b26
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-06-09 09:43:19 +0200

    Rename characterId

--- app/src/main/java/rick_and_morty/Navigator.kt
+++ app/src/main/java/rick_and_morty/Navigator.kt
@@ -7,13 +7,11 @@ import rick_and_morty.eventbus.BusEvent
 import rick_and_morty.eventbus.NavigateToCharacterDetails
 import rick_and_morty.eventbus.NavigateToCharacters
 import rick_and_morty.eventbus.NavigateToEpisodes
-import javax.inject.Inject
-import javax.inject.Singleton
 
 fun NavController.handleNavigation(event: BusEvent) = when (event) {
     is NavigateToCharacterDetails -> {
         val bundle = Bundle().apply {
-            putInt("characterID", event.characterID)
+            putInt("characterID", event.characterId)
         }
         navigate(R.id.viewCharacterDetails, bundle)
     }
--- app/src/main/java/rick_and_morty/eventbus/BusEvent.kt
+++ app/src/main/java/rick_and_morty/eventbus/BusEvent.kt
@@ -2,6 +2,6 @@ package rick_and_morty.eventbus
 
 sealed interface BusEvent
 
-data class NavigateToCharacterDetails(val characterID: Int) : BusEvent
+data class NavigateToCharacterDetails(val characterId: Int) : BusEvent
 object NavigateToEpisodes : BusEvent
 object NavigateToCharacters : BusEvent
