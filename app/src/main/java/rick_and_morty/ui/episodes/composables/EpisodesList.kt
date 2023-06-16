39a566745b14c4cfa2abd1cd435a3687d3b08ced
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2023-06-16 19:44:45 +0200

    Added Ui for Pull to refresh

--- app/src/main/java/rick_and_morty/ui/episodes/composables/EpisodesList.kt
+++ app/src/main/java/rick_and_morty/ui/episodes/composables/EpisodesList.kt
@@ -1,33 +1,33 @@
 package rick_and_morty.ui.episodes.composables
 
-import android.annotation.SuppressLint
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import rick_and_morty.BottomNavigationBar
-import rick_and_morty.eventbus.EventBus
-import rick_and_morty.ui.characters.CharactersViewModel
 import rick_and_morty.ui.episodes.EpisodesViewModel
 import rick_and_morty.ui.widgets.CircularProgressBar
 import rick_and_morty.ui.widgets.RickAndMortyErrorDialog
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun EpisodeList(
         episodesViewModel: EpisodesViewModel = viewModel(modelClass = EpisodesViewModel::class.java),
@@ -35,43 +35,50 @@ fun EpisodeList(
 
         val episodes = episodesViewModel.episodes.collectAsState().value
         val scrollListState = rememberLazyListState()
-        if (episodes.isLoading)
-        {
-                CircularProgressBar()
-        }
-        when {
-                episodes.isFailure -> RickAndMortyErrorDialog(episodes.failure.toString())
-                else -> {
-                        LazyColumn (
-                                modifier = Modifier.background(MaterialTheme.colorScheme.primary),
-                                verticalArrangement = Arrangement.spacedBy(5.dp),
-                                state = scrollListState
-                        ) {
-                                items(episodes.episodeResults) { episode ->
-                                        EpisodesRow(
-                                                episodeResultsDto = episode,
-                                          )
-                                }
-                        }
-                        val userIsAtBottom by remember{
-                                derivedStateOf {
-                                        val layoutInfo = scrollListState.layoutInfo
-                                        val visibleItemsInfo = layoutInfo.visibleItemsInfo
-                                        if (layoutInfo.totalItemsCount == 0) {
-                                                false
-                                        } else {
-                                                val lastVisibleItem = visibleItemsInfo.last()
-                                                val viewportHeight = layoutInfo.viewportEndOffset + layoutInfo.viewportStartOffset
-                                                (lastVisibleItem.index + 1 == layoutInfo.totalItemsCount &&
-                                                        lastVisibleItem.offset + lastVisibleItem.size <= viewportHeight)
+
+        val refreshing = episodes.isLoading
+        val swipeRefreshState = rememberPullRefreshState(refreshing, {episodesViewModel.refreshEpisodes()})
+
+        Box(modifier = Modifier.fillMaxSize().pullRefresh(swipeRefreshState)) {
+                if (episodes.isLoading && episodes.episodeResults.isEmpty()) {
+                        CircularProgressBar()
+                } else {
+                        when {
+                                episodes.isFailure -> RickAndMortyErrorDialog(episodes.failure.toString())
+                                else -> {
+                                        LazyColumn(
+                                                modifier = Modifier.background(MaterialTheme.colorScheme.primary),
+                                                verticalArrangement = Arrangement.spacedBy(5.dp),
+                                                state = scrollListState
+                                        ) {
+                                                items(episodes.episodeResults) { episode ->
+                                                        EpisodesRow(
+                                                                episodeResultsDto = episode,
+                                                        )
+                                                }
+                                        }
+                                        val userIsAtBottom by remember {
+                                                derivedStateOf {
+                                                        val layoutInfo = scrollListState.layoutInfo
+                                                        val visibleItemsInfo = layoutInfo.visibleItemsInfo
+                                                        if (layoutInfo.totalItemsCount == 0) {
+                                                                false
+                                                        } else {
+                                                                val lastVisibleItem = visibleItemsInfo.last()
+                                                                val viewportHeight = layoutInfo.viewportEndOffset + layoutInfo.viewportStartOffset
+                                                                (lastVisibleItem.index + 1 == layoutInfo.totalItemsCount &&
+                                                                        lastVisibleItem.offset + lastVisibleItem.size <= viewportHeight)
+                                                        }
+                                                }
+                                        }
+                                        if (userIsAtBottom) {
+                                                LaunchedEffect(true) {
+                                                        episodesViewModel.getEpisodes()
+                                                }
                                         }
                                 }
                         }
-                        if (userIsAtBottom){
-                                LaunchedEffect(true) {
-                                        episodesViewModel.getEpisodes()
-                                }
-                        }
+                        PullRefreshIndicator(refreshing, swipeRefreshState, Modifier.align(Alignment.TopCenter))
                 }
         }
 }
